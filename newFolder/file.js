b509bae40d55ed446f1ef4bc8b3b782918383074
Daryl Lin <daryllin@Daryls-MacBook-Pro.local>
2017-08-15 17:21:13 -0700

    [change] file.js

--- newFolder/file.js
+++ newFolder/file.js
@@ -1,4 +1,4 @@
-function loopThrough(array, func) {
+function loopThrough (array, func) {
 	for(var index = 0; index < array.length; index++) {
 		var element = array[index];
 		func(element);
@@ -6,11 +6,22 @@ function loopThrough(array, func) {
 }
 
 
-function m(objects, transform) {
+function m (objects, transform) {
 	var final = [];
-	for(var p = 0; p < objects.length; ++p){
+	for (var p = 0; p < objects.length; ++p){
 		var lol = objects[p];
 		final.push(transform(lol));
 	}
 	return final;
+}
+
+function filter (array, callback) {
+	var	result = [];
+	for (var i = 0; i < array.length; i++) {
+		if (callback(array[i])) {
+			result.push(array[i]);
+		}
+	} 
+	return result;
+
 }
